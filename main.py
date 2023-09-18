dae5f3c362d34708077dec4011258d6fd951d8fc
LidiaDomingos <72155007+LidiaDomingos@users.noreply.github.com>
2023-09-18 04:17:23 -0300

    version 2.0

--- main.py
+++ main.py
@@ -1,10 +1,83 @@
 from sys import *
 
+
 class Token:
 
     def __init__(self, type: str, value: int or str):
         self.type = type
         self.value = value
+
+
+class PrePro():
+    def __init__(self) -> None:
+        pass
+
+    def filter(self, string: str):
+        if "//" in string:
+            for i in range(0, len(string)):
+                if string[i] == "/":
+                    if (string[i+1] == "/"):
+                        index = i
+                        break
+            return string[0:index]
+        else:
+            return string
+
+
+class Node():
+    def __init__(self, variant, children):
+        self.variant = variant
+        self.children = children
+
+    def Evaluate(self):
+        pass
+
+
+class BinOp(Node):
+
+    def Evaluate(self):
+        nodeL = self.children[0]
+        nodeR = self.children[1]
+
+        if (self.variant == "+"):
+            result = nodeL.Evaluate() + nodeR.Evaluate()
+
+        elif (self.variant == "-"):
+            result = nodeL.Evaluate() - nodeR.Evaluate()
+
+        elif (self.variant == "/"):
+            result = nodeL.Evaluate() // nodeR.Evaluate()
+
+        elif (self.variant == "*"):
+            result = nodeL.Evaluate() * nodeR.Evaluate()
+
+        return result
+
+
+class UnOp(Node):
+
+    def Evaluate(self):
+        result = self.children
+
+        if (self.variant == "+"):
+            return result.Evaluate()
+
+        elif (self.variant == "-"):
+            return (-1)*result.Evaluate()
+
+
+class IntVal(Node):
+
+    def __init__(self, variant):
+        self.variant = variant
+
+    def Evaluate(self):
+        return self.variant
+
+
+class NoOp(Node):
+    pass
+
 
 class Tokenizer:
 
@@ -52,7 +125,7 @@ class Tokenizer:
             self.token_type = "DIVISION"
             self.next = Token(type=self.token_type,
                               value=self.source[self.position])
-            
+
         elif (self.source[self.position] == "("):
             self.token_type = "START_PARENTHESES"
             self.next = Token(type=self.token_type,
@@ -62,7 +135,7 @@ class Tokenizer:
             self.token_type = "END_PARENTHESES"
             self.next = Token(type=self.token_type,
                               value=self.source[self.position])
-            
+
         elif (self.source[self.position].isdigit()):
             self.token_type = "INT"
             number = ""
@@ -86,81 +159,92 @@ class Parser():
 
     @staticmethod
     def parseTerm():
-        result = Parser().parseFactor()
+        node = Parser().parseFactor()
         while (Parser().tokenizer.next.type == "TIMES" or Parser().tokenizer.next.type == "DIVISION"):
-        
+
             if (Parser().tokenizer.next.type == "TIMES"):
                 Parser().tokenizer.selectNext()
-                resultFactor = Parser().parseFactor()
-                result = result * resultFactor 
-                
+                node2 = Parser().parseFactor()
+                node = BinOp("*", [node, node2])
+
             elif (Parser().tokenizer.next.type == "DIVISION"):
                 Parser().tokenizer.selectNext()
-                resultFactor = Parser().parseFactor()
-                result = result // resultFactor
-        
-        return result
+                node2 = Parser().parseFactor()
+                node = BinOp("/", [node, node2])
+
+        return node
 
     @staticmethod
     def parseExpression():
-        result = Parser().parseTerm()
+        node = Parser().parseTerm()
         while (Parser().tokenizer.next.type == "PLUS" or Parser().tokenizer.next.type == "MINUS"):
             if (Parser().tokenizer.next.type == "PLUS"):
                 Parser().tokenizer.selectNext()
-                resultTerm = Parser().parseTerm()
-                result = result + resultTerm
-                
+                node2 = Parser().parseTerm()
+                node = BinOp("+", [node, node2])
+
             elif (Parser().tokenizer.next.type == "MINUS"):
                 Parser().tokenizer.selectNext()
-                resultTerm = Parser().parseTerm()
-                result = result - resultTerm
-        return result  
+                node2 = Parser().parseTerm()
+                node = BinOp("-", [node, node2])
+
+        return node
 
     @staticmethod
     def parseFactor():
-        result = Parser().tokenizer.next.value
+        node = Parser().tokenizer.next.value
 
         if (Parser().tokenizer.next.type == "INT"):
+            node = IntVal(node)
             Parser().tokenizer.selectNext()
-            return result
-        
+            return node
+
         elif (Parser().tokenizer.next.type == "PLUS"):
             Parser().tokenizer.selectNext()
-            result = Parser().parseFactor()
-            return result
+            nodeFactor = Parser().parseFactor()
+            node = UnOp("+", [nodeFactor])
+            return node
 
         elif (Parser().tokenizer.next.type == "MINUS"):
             Parser().tokenizer.selectNext()
-            result = Parser().parseFactor()           
-            return (-1)*result
-        
+            nodeFactor = Parser().parseFactor()
+            node = UnOp("-", [nodeFactor])
+            return node
+
         elif (Parser().tokenizer.next.type == "START_PARENTHESES"):
             Parser().tokenizer.selectNext()
-            resultExpression = Parser().parseExpression()
+            node = Parser().parseExpression()
 
             if (Parser().tokenizer.next.type != "END_PARENTHESES"):
                 raise SyntaxError("It must have an end parentheses!")
-            
+
             Parser().tokenizer.selectNext()
 
-            return resultExpression
-        
+            return node
+
         else:
             raise SyntaxError("Something is wrong!")
 
     @classmethod
     def run(cls, source: str):
-        cls.tokenizer = Tokenizer(source)
+        str = PrePro().filter(source)
+        cls.tokenizer = Tokenizer(str)
         cls.tokenizer.selectNext()
         result = cls.parseExpression()
         if (Parser().tokenizer.next.type == "EOF"):
-            print(result)
-            return result
+            print(result.Evaluate())
+            return result.Evaluate()
         else:
-            raise SyntaxError("Check if everything is correct! Did not arrive in EOF type")
+            raise SyntaxError(
+                "Check if everything is correct! Did not arrive in EOF type")
+
 
 def main():
-    Parser().run(argv[1])
+
+    with open(argv[1], "r", encoding="utf-8") as file:
+        source = file.read()
+
+    Parser().run(source)
 
 
 if __name__ == "__main__":
